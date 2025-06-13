00feae50b22b5b597e742769b4a2b51ca9407c59
anunyaOrange <anunyaOrange@proton.me>
2025-06-13 21:41:01 +0700

    solution

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +21,26 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-
+app.get('/api/:date', (req, res) => {
+  // const d = req.params.date;
+  const d = parseInt(req.params.date);
+  console.log('req.params.date', d);
+  const date = new Date(d);
+  // const date = new Date(parseInt(req.params.date));
+  // const date = new Date(1451001600000);
+  console.log('date', date);
+  if (!isNaN(date) && date.toString() !== 'Invalid Date') {
+    res.json({
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    });
+  } else {
+    res.json({ error: "Invalid Date" });
+  }
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
